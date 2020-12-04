d9bb389a8dcb498fb425b0736baa88f0340e8414
Riya Romanova <riyaromanova13@gmail.com>
2020-12-04 12:47:31 +0300

    fix(script.js): added book5

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let books = document.querySelectorAll('.book'),
+const books = document.querySelectorAll('.book'),
     bg = document.querySelector('body'),
     bgColor = getComputedStyle(bg),
     headings = document.getElementsByTagName('a'),
@@ -8,8 +8,10 @@ let books = document.querySelectorAll('.book'),
     list = document.querySelector('ul'),
     chapters = list.querySelectorAll('li'),
     listElem = document.createElement('li'),
-    lastList = books[2].querySelector('ul');
-
+    lastList = books[2].querySelector('ul'),
+    list5 = books[5].querySelector('ul'),
+    chapters5 = list5.querySelectorAll('li');
+console.log(chapters5);
 books[0].before(books[1]);
 books[2].before(books[4]);
 books[2].before(books[3]);
@@ -23,8 +25,11 @@ chapters[3].after(chapters[6]);
 chapters[6].after(chapters[8]);
 chapters[5].after(chapters[7]);
 chapters[7].after(chapters[9]);
+chapters5[1].after(chapters5[9]);
+chapters5[9].after(chapters5[3]);
+chapters5[3].after(chapters5[4]);
+chapters5[8].before(chapters5[5]);
 listElem.textContent = 'Глава 8: За пределами ES6';
 lastList.insertAdjacentElement('beforeend', listElem);
-let lastItems = lastList.querySelectorAll('li');
+const lastItems = lastList.querySelectorAll('li');
 lastItems[9].before(lastItems[10]);
-console.log(lastItems);
